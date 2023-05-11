d9a06a2c9ca37a21248d8fd657a6622de4b6d7ee
yashmoyal01 <133185039+yashmoyal01@users.noreply.github.com>
2023-05-11 18:29:38 +0530

    Add files via upload

---
+++ create_tables.py
@@ -0,0 +1,71 @@
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def create_database():
+    """
+    - Creates and connects to the musicadb
+    - Returns the connection and cursor to musicadb
+    """
+    
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=postgres password=yashmoyal")
+    #db_string = f"postgresql://postgres:<password>@127.0.0.1:5432/movie_data"
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    
+    # create musica database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS musicadb")
+    cur.execute("CREATE DATABASE musicadb WITH ENCODING 'utf8' TEMPLATE template0")
+
+    # close connection to default database
+    conn.close()    
+    
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=musicadb user=postgres password=yashmoyal")
+    cur = conn.cursor()
+    
+    return cur, conn
+
+
+def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list. 
+    """
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    """
+    - Drops (if exists) and Creates the musica database. 
+    
+    - Establishes connection with the musica database and gets
+    cursor to it.  
+    
+    - Drops all the tables.  
+    
+    - Creates all tables needed. 
+    
+    - Finally, closes the connection. 
+    """
+    cur, conn = create_database()
+    
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ etl.py
@@ -0,0 +1,114 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+import datetime
+from sql_queries import *
+
+import numpy as np
+from psycopg2.extensions import register_adapter, AsIs
+psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
+
+
+def process_song_file(cur, filepath):
+    '''
+    - Load json meta data
+    - Execute the SQL cursor command to insert artist and song record into tables.           
+    '''
+    
+    # open song file
+    df = pd.read_json(filepath, lines=True, dtype={'year': pd.Categorical})
+
+    # insert song record
+    song_data = list(df.loc[0, ['song_id', 'title', 'artist_id', 'year', 'duration']].values)
+    cur.execute(song_table_insert, song_data)
+    
+    # insert artist record
+    artist_data = list(df.loc[0, ['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values)
+    cur.execute(artist_table_insert, artist_data)
+
+
+def process_log_file(cur, filepath):
+    '''
+    - Load json meta data
+    - Filters data by 'NextSong' action
+    - Convert timestamp column to datetime before extraction
+    - Execute the SQL cursor command to insert time data, user records and songplay records into tables
+    '''
+    
+    # open log file
+    df = pd.read_json(filepath, lines=True)
+
+    # filter by NextSong action
+    df = df.loc[df['page'] == 'NextSong']
+
+    # convert timestamp column to datetime
+    df.ts = df['ts'].apply(lambda ts: datetime.datetime.fromtimestamp(ts/1000.0))
+    
+    # insert time data records
+    time_data = (df['ts'], df['ts'].dt.hour, df['ts'].dt.day, df['ts'].dt.week, df['ts'].dt.month, df['ts'].dt.year, df['ts'].dt.day_name())
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
+
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
+
+    # load user table
+    user_df = df.loc[:, ['userId', 'firstName', 'lastName', 'gender', 'level']]
+
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
+
+    # insert songplay records
+    for index, row in df.iterrows():
+        
+        # get songid and artistid from song and artist tables
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+
+        # insert songplay record
+        songplay_data = (pd.to_datetime(row.ts), row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        cur.execute(songplay_table_insert, songplay_data)
+
+
+def process_data(cur, conn, filepath, func):
+    '''
+    - Iterate through files in filepath and process data with a given function
+    '''
+    
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect("host=127.0.0.1 dbname=musicadb user=postgres password=yashmoyal")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='D:\musica project\song_data', func=process_song_file)
+    process_data(cur, conn, filepath='D:\musica project\log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
